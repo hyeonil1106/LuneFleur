be775947a1448f743f479306f38be07cdc5b9ccc
hyeonil1106 <hyeonil1106@gmail.com>
2026-05-21 12:57:51 +0900

    chore: update vite configuration settings for project build optimization

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/<LuneFleur>/', // 예: 레포지토리 이름이 my-web 이라면 '/my-web/' 으로 작성
+  base: '/LuneFleur/', // 예: 레포지토리 이름이 my-web 이라면 '/my-web/' 으로 작성
 })
